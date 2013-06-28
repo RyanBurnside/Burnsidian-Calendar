f5da61d00e5cd27f9b86cb7ddf041aeb7625704d
ryan <ryan@ryan-server>
2013-06-28 07:18:10 -0600

    Nothing major, Day objects now stored in container

--- cal.py
+++ cal.py
@@ -17,8 +17,10 @@ def make_cal (month, year):
     # Clear the old list of days, prepare to fill with new Day_Data objects
     del cur_days[:]
 
+    # TODO clear all currently existing date buttons (will be container)
+
     # Get number of days to iterate over
-    mrange = calendar.monthrange(year, month)
+    mrange = calendar.monthrange(int(year), int(month))
 
     first_day = mrange[0]
 
@@ -41,10 +43,18 @@ def make_cal (month, year):
 
     # Populate the grid with day buttons
     for r in range(num_days):
+        # Fill list of Day_Data
+        padded_day = str(r + 1)
+        
+        if (len(padded_day) == 1):
+            padded_day = "0" + padded_day
+        cur_days.append(Day_Data(padded_day, month, year,"Dates"))
+
+        # Fill grid with buttons
         d = Tkinter.Button(root, text = '%s\n'%(r + 1),
                            borderwidth = 1, width = 6, height = 2, 
                            activebackground = select_color, 
-                           command = lambda:messageWindow(root, a), 
+                           command = lambda:messageWindow(root, cur_days[r]), 
                            bg = "#ffffff")
 
         if(int(today.strftime("%d")) == r + 1):
@@ -61,11 +71,10 @@ def make_cal (month, year):
                                                           columnspan = 6)
 
 #--------TEST DATUM--------
-print today.strftime("%m") + today.strftime("%Y")
 
 #--------MAIN LOOP BEGIN--------
 root = Tkinter.Tk()
 root.title("Burnsidian Calendar Utility")
 
-make_cal(int(today.strftime("%m")), int(today.strftime("%Y")))
+make_cal(today.strftime("%m"), today.strftime("%Y"))
 root.mainloop()
--- day_form.py
+++ day_form.py
@@ -8,15 +8,16 @@ def messageWindow(parent, day):
     win.title("TODO For ")
     num_lines = day.get_num_chores()
     main_frame = Frame(win, width = 320, height = 240)
-    canvas=Canvas(main_frame, width = 320, height = 240)
-    scroll_bar = Scrollbar(main_frame, orient = VERTICAL)
-    scroll_bar.pack(side = RIGHT, fill = Y)
-    scroll_bar.config(command=canvas.yview)
-    canvas.config(width = 300, height = 240)
-    canvas.config(yscrollcommand=scroll_bar.set)
+    #canvas=Canvas(main_frame, width = 320, height = 240)
+    #scroll_bar = Scrollbar(main_frame, orient = VERTICAL)
+    #scroll_bar.pack(side = RIGHT, fill = Y)
+    #scroll_bar.config(command=canvas.yview)
+    #canvas.config(width = 300, height = 240)
+    #canvas.config(yscrollcommand=scroll_bar.set)
+    print num_lines
 
     for i in range(num_lines):
-        f = Frame(canvas)
+        f = Frame(main_frame)
         b =  Checkbutton(f)
         if(day.get_index(i)[0]):
             b.select()
@@ -26,10 +27,10 @@ def messageWindow(parent, day):
         e.pack(side = LEFT)
         Button(f, text = "Delete").pack(side = LEFT)
         f.pack()
+
     
-    
-    canvas.config(scrollregion = (0,0, 320,))
-    canvas.pack(side = LEFT, expand = TRUE, fill = BOTH)
+    #canvas.config(scrollregion = (0,0, 320,200))
+    #canvas.pack(side = LEFT, expand = TRUE, fill = BOTH)
     main_frame.pack()
 
     x = parent.winfo_x() + 32
